daba6afb97c93b5e63484c9d4bd562355cab83d4
Tfmyself <finalabo@gmail.com>
2026-03-12 14:27:56 +0800

    Add files via upload

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,8 +1,8 @@
 // index.js
 const defaultAvatarUrl = 'https://mmbiz.qpic.cn/mmbiz/icTdbqWNOwNRna42FI242Lcia07jQodd2FJGIYQfG0LAJGFxM4FbnQP6yfMxBgJ0F3YRqJCJ1aPAK2dQagdusBZg/0'
 
-// pages/index/index.js
-const { scoreData } = require('./sourcedata.js'); // 引入数据
+// ！！！关键：从 sourcedata.js 中引入数据（假设这两个文件在同一个文件夹下） ！！！
+const { headers, scoreData } = require('./sourcedata.js');
 
 Page({
   data: {
@@ -12,17 +12,15 @@ Page({
     error: ''
   },
 
-  // 监听输入框
   onInput(e) {
     this.setData({
-      inputVal: e.detail.value.trim(), // 去除前后空格
+      inputVal: e.detail.value.trim(),
       hasSearched: false,
       result: null,
       error: ''
     });
   },
 
-  // 点击查询按钮
   searchScore() {
     const id = this.data.inputVal;
     
@@ -31,12 +29,22 @@ Page({
       return;
     }
 
-    // 在数据中查找
     const found = scoreData.find(item => item.id === id);
 
     if (found) {
+      // 在查询成功时，动态组装出完整的题型得分列表传给前端界面
+      const details = headers.map((name, index) => {
+        return { name: name, score: found.s[index] };
+      });
+
       this.setData({
-        result: found,
+        result: {
+          id: found.id,
+          zc: found.zc,
+          kg: found.kg,
+          zg: found.zg,
+          details: details
+        },
         hasSearched: true,
         error: ''
       });
